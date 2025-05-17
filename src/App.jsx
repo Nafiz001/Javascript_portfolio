e9ab7b4135e9647caf9bbc6715870f897da0564d
Md. Nafiz Ahmed <nafizahmed000@gmail.com>
2025-05-17 19:48:10 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Md. Nafiz Ahmed",
+  title: "Computer Science Student",
+  email: "nafizahmed000",
+  gitHub: "Nafiz001",
+  instagram: "nafiz_ahmed/",
+  linkedIn: "nafiz-ahmed-770a3a273/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Nafizahmed000",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,21 +10,21 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/about_me.avif";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "white abstract background with a hint of color";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a passionate CSE student at KUET, driven by a goal to innovate in technology and software development.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "As a Computer Science and Engineering student at Khulna University of Engineering & Technology (KUET), I am deeply enthusiastic about exploring algorithms, software development, and emerging technologies. Hailing from Bogura, Bangladesh, and having completed my HSC at Notre Dame College, I aim to leverage my skills to create impactful solutions in the tech industry. My passion lies in mastering complex problems through coding and contributing to open-source projects, with a long-term goal of advancing AI and software systems. A quote that inspires me is: “The only way to do great work is to love what you do.” – Steve Jobs ";
 
 const About = () => {
   return (
