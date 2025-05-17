eb60cf1e5f78a4c4c5026ec1ad8522d096445774
Md. Nafiz Ahmed <nafizahmed000@gmail.com>
2025-05-17 21:07:15 +0000

    education

---
+++ src/Components/Education.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <div className="education">
+      <h2>Education</h2>
+      <ul>
+        <li>
+          <strong>Khulna University of Engineering & Technology</strong> - Bachelor of Science in
+          Computer Science
+          <br />
+          Graduated: June 2027
+        </li>
+        <li>
+          <strong>Coursera</strong> - Machine Learning Specialization
+          <br />
+          Completed: December 2025
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Education;
